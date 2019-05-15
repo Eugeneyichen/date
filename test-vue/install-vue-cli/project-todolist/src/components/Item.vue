9900d1d9b831b1c4b986221877fc5bdc17c99745
eugene <1160178908@qq.com>
2019-05-15 18:13:51 +0800

    0515 frist

---
+++ test-vue/install-vue-cli/project-todolist/src/components/Item.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="Item">
+		<input type="checkbox" v-model="todo.done">
+		<span>{{todo.task}}</span>
+		<button>删除</button>
+	</div>
+</template>
+<script>
+	export default {
+		name:'Item',
+		props:{
+			todo:Object
+		}
+	}
+</script>
+<style scoped>
+	.Item{
+		width: 100%;
+		line-height: 40px;
+		border:1px solid #ccc;
+	}
+	.Item input{
+		float: left;
+		margin-top: 14px;
+		margin-right: 10px;
+	}
+	.Item button{
+		float: right;
+		margin-top: 10px;
+	}
+</style>
