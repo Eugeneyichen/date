8807670a802f5c4be20c8fbf75c4aa06d3d63464
eugene <1160178908@qq.com>
2019-05-17 18:49:41 +0800

    0517 night

--- mshop/home/src/App.vue
+++ mshop/home/src/App.vue
@@ -1,24 +1,24 @@
-<!-- 1.页面-->
+<!--1.页面-->
 <template>
-	<div class="App" id="app">
-		hello
-		<p>hasha</p>
+	<div class="App" id='app'>
+		<router-view></router-view>
+		<TabBar />
 	</div>
 </template>
-
-<!-- 2.逻辑-->
+<!--2.逻辑-->
 <script>
-	//2.1引入组件
+	//2.1 引入组件
+	import TabBar from './components/TabBar/TabBar'
 	export default {
 		name:'App',
+		components:{
+			TabBar
+		}
 	}
 </script>
-
-<!-- 3.样式-->
+<!--3.样式-->
 <style lang="less">
 	#app{
-		p{
-			color:red;
-		}
+		.rem(padding-bottom,60)
 	}
 </style>
--- mshop/home/src/store/actions.js
+++ mshop/home/src/store/actions.js
@@ -1,4 +1,21 @@
+/*
+* @Author: TomChen
+* @Date:   2019-05-13 20:42:57
+* @Last Modified by:   TomChen
+* @Last Modified time: 2019-05-15 18:35:59
+*/
+import {
+	GET_HOME_PRODUCTS
+} from './types.js'
+
+import {
+	getHomeProducts
+} from '../api'
 
 export default {
-	
+	async [GET_HOME_PRODUCTS]({commit}){
+		const products = await getHomeProducts()
+		console.log(products)
+		commit(GET_HOME_PRODUCTS,{homeProducts:products})
+	}
 }
